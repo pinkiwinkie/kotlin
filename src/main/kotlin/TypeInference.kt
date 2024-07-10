e63f650bd3250b9082e551fba645aae4f240f795
pinkiwinkie <elrumor99@gmail.com>
2024-07-10 14:32:29 +0200

    variables II

--- src/main/kotlin/TypeInference.kt
+++ src/main/kotlin/TypeInference.kt
@@ -7,4 +7,17 @@ fun main() {
     var b = false // kotlin deduces the boolean data type from the value
     var s = "Amigoscode" // kotlin deduces the string data type from the value
     var c = 'A'
+
+    //3. Declaration of variables only indicating the type
+    val x: Int
+    val isBlue: Boolean
+    val surnames: String
+
+    //4. Try to modify some previous variable
+        //surnames = "hola" -> This causes a compile time error
+        // * explained at ValvsVar.kt *
+
+    //5. Try to assign null to some variable
+        //val marca: String = null -> This causes a compile time error
+        // * explained at Nulls.kt *
 }
--- src/main/kotlin/ValVsVar.kt
+++ src/main/kotlin/ValVsVar.kt
@@ -5,4 +5,13 @@ fun main() {
     // final String brand = "Amigoscode"
     val brandVal = "Amigoscode"
     // brandVal = "CodeWithAmigoscode"
+
+    //6. VAL is used to declare read-only variables, that is, not mutable
+    // If we want to define mutable variables we must use VAR
+    //7. When should we use val or var?
+        // Actually, we can always use var and there would be no problems in the project,
+        // but the reality is that programming is a work that is done as a team and
+        // val serves to give semantics to the code that is written.
+        // Being able to know at a glance whether a variable will mutate or not throughout the code
+        // it is very useful for the rest of the team working on the same project.
 }
